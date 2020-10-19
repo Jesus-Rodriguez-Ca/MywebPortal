5007a6f53ee651cd8a6678fd457822070f6f1aa0
Jesus Rodriguez <53617197+Jesus-Rodriguez-Ca@users.noreply.github.com>
2020-10-18 21:14:59 -0700

    Add form and PHP portal conig

--- big/includes/footer.php
+++ big/includes/footer.php
@@ -1,7 +1,7 @@
 <!-- Footer starts here-->
      
      <footer>
-      <p><small>&copy; 2018 - <?=date('Y')?> by <a href="contactme.php">Contact Jesus Rodriguez </a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+      <p><small>&copy; 2020 by <a href="contact.php">Contact Jesus Rodriguez </a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
     </footer>
   </div>
      
--- includes/header.php
+++ includes/header.php
@@ -1,25 +1,37 @@
+<?php include 'portal-config.php'?>
 <!DOCTYPE html>
 <html lang="en">
  <head>
-     <title>Jesus Rodriguez: IT162 Portal Website</title>
+     <title><?=$title?></title>
   <meta charset="utf-8" />
   <meta name="robots" content="noindex,nofollow" />
   <meta name="viewport" content="width=device-width" />
   <link rel="stylesheet" href="css/portal.css" />
-  <link rel="stylesheet" href="css/nav.css" />
+     <link rel="stylesheet" href="css/forms.css">
+     <link rel="stylesheet" href="css/nav.css">
+    <script src="https://use.fontawesome.com/6a71565c22.js"></script>
+     
+  
  </head>
  <body>
      <header>
-     <h1>Jesus Rodriguez: IT162 Portal Website</h1>
-    <nav class="topnav" id="myTopnav">
-       <a href="index.html" class="active">Welcome</a>
-       <a href="big/index.php">Big</a>
-       <a href="aia.php" >AIA</a>
-       <a href="flowchart.php">Flowchart</a>
-       <a href="fp/index.php">Final Project</a>
-       <a href="contactme.php">Contact Jesus</a>
-       <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
-    </nav>
+     <h1><i class="logo fa <?=$logo?>"<?=$logo_color?>></i>Jesus' SCC IT162 Portal</h1>
+     <nav>
+    <ul class="topnav" id="myTopnav">
+    <?= makeLinks($nav1)?>    
+    <!-- START COMMENT OUT NAV
+      <li><a href="index.php" class="selected">Welcome</a></li>
+      <li><a href="big/index.php">Big</a></li>
+      <li><a href="aia.php">AIA</a></li>
+      <li><a href="flowchart.php">Flowchart</a></li>
+      <li><a href="fp/index.php">Final Project</a></li>
+      <li><a href="contactme.php">Contact Jesus</a></li>
+	END COMMENT OUT NAV -->
+     <li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
+    </ul>
+  </nav>
+  </nav>
+
      </header>
      
    <div class="wrapper">
